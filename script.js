2f9cec8065010ea8e2c36bde0ae73c690733683c
A <abdibekele94@gmail.com>
2022-11-02 15:05:51 -0400

    Fix storage button

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-class Book{
+let books;
+
+class collection{
     constructor(title,author)
     {
         this.title=title;
@@ -10,28 +12,75 @@ class UI {
         const books = Store.getBooks();
         books.forEach((book)=>UI.addBookToList(book));
     }
-    static addBookToList(book) {
-        const list =document.querySelector('.bookLists');
-        const row =document.createElement('tr');
-        row.innerHTML =
-        `<td id="authorTitle"> <div class="raw-lists-header"> <h3>"${book.title}" by </h3>
-        <h3>${book.author}</h3></td> </div>
-      
-        `;
-        const createButton = document.createElement('td');
-    const removeBtn = document.createElement('button');
-    removeBtn.classList.add('remove-button');
-    removeBtn.type = 'submit';
-    removeBtn.innerText = 'Remove';
-    createButton.appendChild(removeBtn);
-    row.appendChild(createButton);
-        list.appendChild(row);
 
+    static addBookToList(book) {
+      const list = document.querySelector('.bookLists');
+  
+      const row = document.createElement('tr');
+      row.classList.add('row');
+  
+      const createTitle = document.createElement('th');
+      const createAuthor = document.createElement('th');
+      createTitle.innerHTML = book.title;
+      createAuthor.innerHTML = book.author;
+      const createButton = document.createElement('th');
+      const removeBtn = document.createElement('button');
+      removeBtn.classList.add('remove-button');
+      removeBtn.type = 'submit';
+      removeBtn.innerText = 'Remove';
+      createButton.appendChild(removeBtn);
+  
+      row.appendChild(createTitle);
+      row.appendChild(createAuthor);
+      row.appendChild(createButton);
+      list.appendChild(row);
     }
-    static clearField(){
-           document.querySelector('#title').value="";
-        document.querySelector('#author').value="";
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // static addBookToList(book) {
+    //     const list =document.querySelector('.bookLists');
+    //     const row =document.createElement('tr');
+    //     row.innerHTML =
+    //     `<td">  "${book.title}"
+    //     </td> 
+    //     <td>${book.author}</td>
+      
+    //     `;
+    //     const createButton = document.createElement('td');
+    //     row.classList.add('lists-container')
+    // const removeBtn = document.createElement('button');
+    // removeBtn.classList.add('remove-button');
+    // removeBtn.type = 'submit';
+    // removeBtn.innerText = 'Remove';
+    // createButton.appendChild(removeBtn);
+    // row.appendChild(createButton);
+    //     list.appendChild(row);
+
+    // }
+
+
+
+
+    // static clearField(){
+    //        document.querySelector('#title').value="";
+    //     document.querySelector('#author').value="";
+    // }
+    //<td id="authorTitle"> <div class="raw-lists-header"> <h3>"${book.title}" by </h3>
+  //  <h3>${book.author}</h3></div </td> 
 
     static deleteBook(el) {
         if (el.classList.contains('remove-button')) {
@@ -63,17 +112,16 @@ class Store {
         localStorage.setItem('books',JSON.stringify(books));
 
     }
-    static removeBook(author)
-    {
-        const books=Store.getBooks();
-        books.forEach((book,index)=>{
-            if(book.author == author)
-            {
-              books.splice(index,1);
-            }
-        })
-        localStorage.setItem('books',JSON.stringify(books));
-        
+    static removeBook(author) {
+      const books = Store.getBooks();
+  
+      books.forEach((book, index) => {
+        if (book.author === author) {
+          books.splice(index, 1);
+        }
+      });
+  
+      localStorage.setItem('books', JSON.stringify(books));
     }
 }
 
@@ -88,16 +136,17 @@ class Store {
       const author =document.querySelector('#author').value;
       //instantiate
 
-      const book =new Book(title,author);
+      const book =new collection(title,author);
       //Add book to list
       UI.addBookToList(book);
       Store.addBook(book);
-      UI.clearField();
+        document.querySelector('#title').value="";
+      document.querySelector('#author').value="";
+    //   UI.clearField();
     Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
     
-      // document.querySelector('#title').value="";
-      // document.querySelector('#author').value="";
+    
   })
 
   //Event :Remove a Book
@@ -107,7 +156,6 @@ class Store {
   
 
   });
-
 
 
 
